12f6f53d897beeeeee5d574da97644df13c65352
marcus.salinas <12.marcus.salinas@gmail.com>
2021-08-03 13:34:05 +0400

    init

---
+++ lib/dealervault_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dealervault_api/version"
+
+module DealervaultApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/dealervault_api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe DealervaultApi do
+  it "has a version number" do
+    expect(DealervaultApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
